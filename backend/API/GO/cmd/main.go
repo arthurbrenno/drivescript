cfafdf80505ac1317c3a057c43a6e21afa1c6329
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-12-03 23:22:16 -0300

    update

--- backend/API/GO/cmd/main.go
+++ backend/API/GO/cmd/main.go
@@ -1,17 +1,93 @@
 package main
 
 import (
-    "log"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
+type Claims struct {
+	UserID         int    `json:"userid"`
+	NivelPermissao int    `json:"nivel_permissao"`
+	jwt.StandardClaims
+}
+
+type UserData struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
 func main() {
-    app := fiber.New()
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/drivescript")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app := fiber.New()
 
-    log.Fatal(app.Listen(":3000"))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Post("/api/login", func(c *fiber.Ctx) error {
+		var userData UserData
+		if err := c.BodyParser(&userData); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Erro ao analisar os dados do usuário",
+			})
+		}
+
+		var userID int
+		var nivelPermissao int
+		err := db.QueryRow("SELECT id, nivel_perm, password FROM usuarios WHERE username = ?", userData.User).Scan(&userID, &nivelPermissao)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"error": "Credenciais inválidas",
+				})
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Erro ao consultar o banco de dados",
+			})
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(userData.Password), []byte(passwordFromDB)) 
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Credenciais inválidas",
+			})
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+			UserID:         userID,
+			NivelPermissao: nivelPermissao,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+			},
+		})
+
+		tokenString, err := token.SignedString([]byte("sua_chave_secreta_aqui"))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Erro ao criar o token",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"auth":    true,
+			"token":   tokenString,
+			"usuario": fiber.Map{"user": userData.User, "password": ""}, // A senha nunca deve ser enviada ao cliente
+		})
+	})
+
+	// Altere a porta conforme necessário
+	port := 3000
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
